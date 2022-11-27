177ecf3c7c979aa62e2b4ebfe7d31582b0f79f1c
Sosssen <jakub.sosnowski2001@gmail.com>
2022-11-27 05:01:06 +0100

    Load and display lenna image

--- image_filtering/Form1.Designer.cs
+++ image_filtering/Form1.Designer.cs
@@ -29,20 +29,52 @@ namespace image_filtering
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.Canvas, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1574, 1129);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Canvas
+            // 
+            this.Canvas.Location = new System.Drawing.Point(3, 3);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(781, 1123);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1574, 1129);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox Canvas;
     }
 }
 
--- image_filtering/Form1.cs
+++ image_filtering/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,89 @@ namespace image_filtering
 {
     public partial class Form1 : Form
     {
+        private DirectBitmap db = null;
+        private Bitmap image = null;
+
+        private static int histogramSize = 256;
+        private int[] histogramRed = new int[histogramSize];
+        private int[] histogramGreen = new int[histogramSize];
+        private int[] histogramBlue = new int[histogramSize];
         public Form1()
         {
             InitializeComponent();
+
+            Configuration();
+        }
+
+        public void Configuration()
+        {
+            Rectangle screen = Screen.PrimaryScreen.WorkingArea;
+            int w = (int)(screen.Width / 1.25);
+            int h = (int)(screen.Height / 1.25);
+            this.Size = new Size(w, h);
+
+            string filename = @".\lib\lenna.png";
+            LoadImage(filename);
+        }
+
+        public void LoadImage(string filename)
+        {
+            image = new Bitmap(filename);
+            db = new DirectBitmap(image.Width, image.Height);
+            using (Graphics g = Graphics.FromImage(db.Bitmap))
+            {
+                g.DrawImage(image, 0, 0);
+            }
+            Canvas.Width = image.Width;
+            Canvas.Height = image.Height;
+            Canvas.Image = db.Bitmap;
+            Canvas.Invalidate();
+            Canvas.Update();
+        }
+    }
+
+    public class DirectBitmap : IDisposable
+    {
+        public Bitmap Bitmap { get; private set; }
+        public Int32[] Bits { get; private set; }
+        public bool Disposed { get; private set; }
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        protected System.Runtime.InteropServices.GCHandle BitsHandle { get; private set; }
+
+        public DirectBitmap(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Bits = new Int32[width * height];
+            BitsHandle = System.Runtime.InteropServices.GCHandle.Alloc(Bits, System.Runtime.InteropServices.GCHandleType.Pinned);
+            Bitmap = new Bitmap(width, height, width * 4, System.Drawing.Imaging.PixelFormat.Format32bppPArgb, BitsHandle.AddrOfPinnedObject());
+        }
+
+        public void SetPixel(int x, int y, Color colour)
+        {
+            int index = x + (y * Width);
+            int col = colour.ToArgb();
+
+            Bits[index] = col;
+        }
+
+        public Color GetPixel(int x, int y)
+        {
+            int index = x + (y * Width);
+            int col = Bits[index];
+            Color result = Color.FromArgb(col);
+
+            return result;
+        }
+
+        public void Dispose()
+        {
+            if (Disposed) return;
+            Disposed = true;
+            Bitmap.Dispose();
+            BitsHandle.Free();
         }
     }
 }
